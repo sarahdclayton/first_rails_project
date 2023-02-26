ef062ae6c2fe4237dfc85339e0a2955b5c2a883a
sdclayton <96846005+sdclayton@users.noreply.github.com>
2023-02-26 12:42:42 -0600

    added: about controller & view, main controller w/ index view, routes, bootstrap, partical navbar & flash message

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root to: "main#index" 
+  # GET /about 
+  get 'about', to: "about#index"
 end
