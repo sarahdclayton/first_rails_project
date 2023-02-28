901826015c03a302a290f72fb5756a4e0287760f
sdclayton <96846005+sdclayton@users.noreply.github.com>
2023-02-27 20:29:13 -0600

    Commit for Class Work

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@
 class User < ApplicationRecord
     has_secure_password
     validates :email, presence: true, format: { with: /\A[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}\z/, message: "must be a valid email address" }
+    #validates :email, format : {with: URI::MailTo::Email_REGEXP}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,9 @@ Rails.application.routes.draw do
   # GET /sign_up
   get 'sign_up', to: "registrations#new"
   post 'sign_up', to: "registrations#create"
-  delete 'logout', to: "session#destroy"
+
+  get 'sign_in', to: "sessions#new"
+  post 'sign_in', to: "sessions#create"
+
+  # delete 'logout', to: "sessions#destroy"
 end
