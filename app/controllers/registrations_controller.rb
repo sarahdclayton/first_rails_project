779c8aa2e2e4c982a6cefd8f7b4986f96ef019b3
sdclayton <96846005+sdclayton@users.noreply.github.com>
2023-02-27 18:30:15 -0600

    working on logout button

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -6,7 +6,11 @@ class RegistrationsController < ApplicationController
 
     def create 
         @user = User.new(user_params)
+        # this signs in user...runs validation and inserts to database. 
         if @user.save
+            # setting a 'session' cookie to keep track of user being signed in
+            # session gets encrypted to get stored as a  cookie
+            # add user id into the session as database id
             session[:user_id] = @user.id 
             redirect_to root_path, notice: "Successfully created account"
         else
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   # GET /sign_up
   get 'sign_up', to: "registrations#new"
   post 'sign_up', to: "registrations#create"
+  delete 'logout', to: "session#destroy"
 end
