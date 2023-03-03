21e18c77f8de17d167336eac1c88e59f479f1d3b
sdclayton <96846005+sdclayton@users.noreply.github.com>
2023-03-03 08:23:22 -0600

    fixed logout issue

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,8 +13,8 @@ class SessionsController < ApplicationController
         end
     end
 
-    # def destroy 
-    #     session[:user_id] = nil
-    #     redirect_to root_path, notice "Logged Out"
-    # end
+    def destroy 
+        session[:user_id] = nil
+        redirect_to root_path, notice: "Logged Out"
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,12 @@ Rails.application.routes.draw do
   # GET /sign_up
   get 'sign_up', to: "registrations#new"
   post 'sign_up', to: "registrations#create"
+  delete 'logout', to: "sessions#destroy"
 
+  # sign_in form
   get 'sign_in', to: "sessions#new"
+  # sign_in submission
   post 'sign_in', to: "sessions#create"
 
-  # delete 'logout', to: "sessions#destroy"
+  
 end
